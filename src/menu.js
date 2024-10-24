f1911cd1f7b9c9e86e97d5a9d50658736b11d331
Favour <favouramike@gmail.com>
2024-10-24 16:05:58 +0100

    Create menu category containers and titles

--- src/menu.js
+++ src/menu.js
@@ -12,6 +12,29 @@ export const menu = ( function() {
 
     menu__content.appendChild(menu__title);
     menu__content.appendChild(menu__grid);
+
+    //menu categories
+    const menu__category__bread = document.createElement('div');
+    menu__category__bread.classList.add('menu__category--title');
+    menu__category__bread.textContent = "Bread";
+
+    const menu__category__cakes = document.createElement('div');
+    menu__category__cakes.classList.add('menu__category--title');
+    menu__category__cakes.textContent = "Cakes"; 
+
+    const menu__category__drinks = document.createElement('div');
+    menu__category__drinks.classList.add('menu__category--title');
+    menu__category__drinks.textContent = "Drinks";
+
+    const menu__category__special = document.createElement('div');
+    menu__category__special.classList.add('menu__category--title');
+    menu__category__special.textContent = "Specials";
+
+    menu__content.appendChild(menu__category__bread);
+    menu__content.appendChild(menu__category__cakes);
+    menu__content.appendChild(menu__category__drinks);
+    menu__content.appendChild(menu__category__special);
+
     content.appendChild(menu__content);
     
 
