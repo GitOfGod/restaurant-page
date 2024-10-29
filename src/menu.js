e67d725f0f29ccf5102d3b3330ba1a2ea0e4d75a
Favour <favouramike@gmail.com>
2024-10-29 15:14:36 +0000

    Refactor menu function using createAnElement()
    - Import createAnElement function into menu module
    - Refactor elements and their text content using the function

--- src/menu.js
+++ src/menu.js
@@ -1,56 +1,30 @@
+import { createAnElement } from "./createAnElement";
+
 export const menu = ( function() {
     const content = document.querySelector('#content');
-    const menu = document.createElement('div');
-    const menu__content = document.createElement('div');
-    const menu__title = document.createElement('h2');
-    const menu__grid = document.createElement('div');
-
-    menu.className = 'menu';
-    menu__content.classList.add('menu__content');
-    menu__title.classList.add('menu__title');
-    menu__grid.classList.add('menu__grid');
-
-    menu__title.textContent = "Menu";
-
-    menu__content.appendChild(menu__title);
-    menu__content.appendChild(menu__grid);
+    const menu = createAnElement(content, 'div','menu');
+    const menu__content = createAnElement(menu, 'div','menu__content');
+    const menu__title = createAnElement(menu__content,'h2','menu__title', 'Menu');
+    const menu__grid = createAnElement(menu__content,'div', 'menu__grid');
 
     //menu categories
     function createMenuCategory(category) {
-        const categoryContainer= document.createElement('div');
-        categoryContainer.classList.add('menu_category');
-
-        const categoryTitle = document.createElement('h3');
-        categoryTitle.classList.add('menu__category--title');
-        categoryTitle.textContent = category;
-        categoryContainer.appendChild(categoryTitle);
-        menu__grid.appendChild(categoryContainer);
+        const categoryContainer = createAnElement(menu__grid,'div','menu_category');
+        const categoryTitle = createAnElement(categoryContainer,'h3','menu__category--title', category);
         return categoryContainer;
+    }
 
+    function createMenuItem(category, title, description, price) {
+        const menu_item = createAnElement(category,'div','menu__item');
+        const item_title = createAnElement(menu_item,'h4','menu__item--title', title);
+        const item_desc = createAnElement(menu_item,'p','menu__item--desc', description);
+        const item_price = createAnElement(menu_item,'p','menu__item--price',price);
     }
 
     const bread_category = createMenuCategory("Bread");
     const cakes_category = createMenuCategory("Cakes");
     const drinks_category = createMenuCategory("Drinks");
     const special_category = createMenuCategory("Specials");
-
-    function createMenuItem(category, title, description, price) {
-        const menu_item = document.createElement('div');
-        const item_title = document.createElement('h4');
-        const item_desc = document.createElement('p');
-        const item_price = document.createElement('p');
-        menu_item.classList.add('menu__item');
-        item_title.classList.add('menu__item--title');
-        item_desc.classList.add('menu__item--desc');
-        item_price.classList.add('menu__item--price');
-        item_title.textContent = title;
-        item_desc.textContent = description;
-        item_price.textContent = price;
-        menu_item.appendChild(item_title);
-        menu_item.appendChild(item_desc);
-        menu_item.appendChild(item_price);
-        category.appendChild(menu_item);
-    }
 
     createMenuItem(bread_category, 'White Loaf', 'Our bread is made from the finest flour', '£4.75');
     createMenuItem(bread_category, 'Tiger Loaf', 'Our bread is made from the finest flour', '£4.75');
@@ -63,12 +37,5 @@ export const menu = ( function() {
     createMenuItem(drinks_category, 'Americano', 'Our drinks are made from quality ingredients', '£2.99');
     
     createMenuItem(special_category, 'Ekima Special', 'Our drinks are made from quality ingredients', '£5.99');
-
-
-
-    menu.appendChild(menu__content);
-    content.appendChild(menu);
-
-
 }
 )();
