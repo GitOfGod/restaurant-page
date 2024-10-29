1fae12e575e7d4258fc1d15fe9c9fbe84a80ac39
Favour <favouramike@gmail.com>
2024-10-29 22:28:17 +0000

    Export contactPage function from contact module
    - Create contact page header, phone, address and content containers and
    append to the content element
    - Import contactPage function into index module for DOM rendering
    - Set minimum and maximum heigh of content container
    - Set contact minimum width to 100% of viewport
    - Use the same styling values for menu content for contact content
    -

--- src/contact.js
+++ src/contact.js
@@ -1,1 +1,22 @@
-import contactImage from './contact_us.jpg';
+/*import contactImage from './contact_us.jpg'; */
+import { createAnElement } from "./createAnElement";
+
+
+export const contactPage = ( function() {
+    const content = document.querySelector('#content');
+
+    const contact = createAnElement(content,'div', 'contact');
+    const contact_content = createAnElement(contact, 'div', 'contact_content');
+    const contactTitle = createAnElement(contact_content,'h3', 'contact__title', 'Contact Us');
+    const phone = createAnElement(contact_content,'div', 'phone');
+    const address = createAnElement(contact_content, 'div', 'address');
+
+    
+
+
+
+
+    
+}
+
+)();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./styles.css";
 /**import "./homepage";
 import "./menu"; **/
+import "./contact";
 console.log("Connection working");
