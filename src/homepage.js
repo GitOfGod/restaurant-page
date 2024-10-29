2bb6eb58ec971274a5c180db3d399f71a10ecb52
Favour <favouramike@gmail.com>
2024-10-29 13:27:27 +0000

    Refactor homepage function using createAnElement()
    - Import createAnElement function into homepage module
    - Create main container, header and headline using the function
    - Increase width of the main to 100%

--- src/homepage.js
+++ src/homepage.js
@@ -1,19 +1,10 @@
+import { createAnElement } from "./createAnElement";
+
 export const homepage = ( function() {
     const content = document.querySelector("#content");
-    const main = document.createElement("div");
-    main.classList.add("main");
-
-    const h1 = document.createElement("h1")
-    h1.textContent = "Baked Haven";
-
-    const p = document.createElement("p");
-    p.classList.add("main--headline");
-    p.textContent = "A place you can have a healthy relationship with Food";
-
-    main.appendChild(h1);
-    main.appendChild(p);
-    content.appendChild(main);
-
+    const main = createAnElement(content, 'div', 'main');
+    const h1 = createAnElement(main, "h1", null, "Baked Haven");
+    const p = createAnElement(main, "p", "main--headline", "A place you can have a healthy relationship with Food");
 }
 
 )();
