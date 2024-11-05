709c23f47cd41d153c2748850d0793d5815b2d62
Favour <favouramike@gmail.com>
2024-11-05 15:23:21 +0000

    Import map image and add to address container

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,5 @@
 import contactImage from './contact_us.jpg';
+import mapImage from './generic-city-map.jpg';
 import { createAnElement } from "./createAnElement";
 
 
@@ -8,16 +9,22 @@ export const contactPage = ( function() {
     const contact_content = createAnElement(contact, 'div', 'contact_content');
     const contactTitle = createAnElement(contact_content,'h3', 'contact__title', 'Contact Us');
     const contactMain = createAnElement(contact_content,'div','contact__main');
+
     const phoneAddress = createAnElement(contactMain,'div', 'phoneAddress');
     const phone = createAnElement(phoneAddress,'div', 'phone');
     const phoneTitle = createAnElement(phone, 'h4', 'phone__title', 'Phone Number');
     const phoneNumber = createAnElement(phone, 'p', 'phone__number', '020 1234 5678');
+
     const address = createAnElement(phoneAddress, 'div', 'address');
     const addressTitle = createAnElement(address, 'h4', 'address_title', 'Address');
     const addressContent = createAnElement(address, 'p', 'address_content', '1 Anywhere Street, Anycity, London');
+
     const imageContainer = createAnElement(contactMain, 'div', 'imageContainer');
     const image = createAnElement(imageContainer,'img', 'contactImg');
     image.src = contactImage;
+
+    const map = createAnElement(address, 'img', 'mapImg',)
+    map.src = mapImage;
     
 }
 
