816df4bbfea14e20981f93037355263d25a91c6a
Favour <favouramike@gmail.com>
2024-11-05 13:07:16 +0000

    Refactored styles for menu and contact components
    - Changed menuTitle class to menu__title to maintain consistency and
    improve readability
    
    - Added shared styling for `.menu__title` and `.contact__title`
    to maintain consistent appearance
    - Enhanced `.contact__main` to match layout and padding of `.menu__grid`
    - Introduced `.imageContainer` class for flexible image alignment within
     contact section
    - Adjusted contact image dimensions and layout for improved visual
    consistency

--- src/contact.js
+++ src/contact.js
@@ -8,11 +8,12 @@ export const contactPage = ( function() {
     const contact = createAnElement(content,'div', 'contact');
     const contact_content = createAnElement(contact, 'div', 'contact_content');
     const contactTitle = createAnElement(contact_content,'h3', 'contact__title', 'Contact Us');
-    const contactMain = createAnElement(contact_content,'div','contactMain');
+    const contactMain = createAnElement(contact_content,'div','contact__main');
     const phoneAddress = createAnElement(contactMain,'div', 'phoneAddress');
     const phone = createAnElement(phoneAddress,'div', 'phone');
     const address = createAnElement(phoneAddress, 'div', 'address');
-    const image = createAnElement(contactMain,'img');
+    const imageContainer = createAnElement(contactMain, 'div', 'imageContainer');
+    const image = createAnElement(imageContainer,'img', 'contactImg');
     image.src = contactImage;
     
 }
